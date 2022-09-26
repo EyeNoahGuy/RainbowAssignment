0f328043f63f7809410eea63eea7c8e59e3b2b47
EyeNoahGuy <103152418+EyeNoahGuy@users.noreply.github.com>
2022-09-26 14:23:45 -0400

    Update ColorBlock.js

--- ColorBlock.js
+++ ColorBlock.js
@@ -3,8 +3,8 @@ import React from 'react'
 function ColorBlock(prop){
     return (
         <div classaName="colorBlock"
-        style={{'backgroundColor': props.color}}>
-            <p>{props.color}</p>
+        style={{'backgroundColor': prop.color}}>
+            <p>{prop.color}</p>
         </div>
     )
 }
